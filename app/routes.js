1892f500716d3b14ea077381adb0354403d89b2e
Harish Khattri <harishkhattri@gmail.com>
2016-03-30 20:51:36 +0530

    Implemented back-end API for all five requirements

--- app/routes.js
+++ app/routes.js
@@ -5,6 +5,10 @@ var Projects = require("./models/project");
 
 module.exports = function(app) {
 	// Server routes
+	app.get('/favicon.ico', function(request, response) {
+		response.writeHead(200, {'Content-Type': 'image/x-icon'} );
+		response.end();
+	});
 	
 	// Get total number of MW
 	app.get('/projects/getTotalMW', function(request, response) {
@@ -23,7 +27,49 @@ module.exports = function(app) {
 	app.get('projects/getMWByFuelType', function(request, response) {
 		Projects.aggregate(
 				{ $group: {_id: {fuelType: "$fuelType"}, totalMW: {$sum: "$mw"} } },
-				{ $project: {_id: 0, fuelType: "$_id.fuelType", totalMW: "$totalMW"} },
+				{ $project: {_id: 0, fuelType: "$_id.fuelType", totalMW: 1} },
+				function(error, result) {
+					if (error) {
+						response.send(error);
+					}
+					
+					response.send(result);
+				});
+	});
+	
+	// Get project size (in MW) by interconnection status
+	app.get('projects/getMWByStatus', function(request, response) {
+		Projects.aggregate(
+				{ $group: {_id: {status: "$status"}, totalMW: {$sum: "$mw"} } },
+				{ $project: {_id: 0, status: "$_id.status", totalMW: 1} },
+				function(error, result) {
+					if (error) {
+						response.send(error);
+					}
+					
+					response.send(result);
+				});
+	});
+	
+	// Get project size (in MW) and count by state
+	app.get('projects/getMWByState', function(request, response) {
+		Projects.aggregate(
+				{ $group: {_id: {state: "$state"}, totalMW: {$sum: "$mw"}, count: {$sum: 1} } },
+				{ $project: {_id: 0, state: "$_id.state", totalMW: 1, count: 1} },
+				function(error, result) {
+					if (error) {
+						response.send(error);
+					}
+					
+					response.send(result);
+				});
+	});
+	
+	// Get project size (in MW) and count by county
+	app.get('projects/getMWByCounty', function(request, response) {
+		Projects.aggregate(
+				{ $group: {_id: {county: "$county"}, totalMW: {$sum: "$mw"}, count: {$sum: 1} } },
+				{ $project: {_id: 0, county: "$_id.county", totalMW: 1, count: 1} },
 				function(error, result) {
 					if (error) {
 						response.send(error);
